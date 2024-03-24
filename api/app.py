b175f2e122ae7ddd2bc6f7cb5a646062192c939c
Ankan Halder <hanlderankan95@gmail.com>
2024-03-24 16:26:34 +0530

    add prediction support

--- api/app.py
+++ api/app.py
@@ -1,9 +1,61 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request as req
+from flask_cors import CORS
+from gradio_client import Client,file
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+
+
+app = Flask(__name__)
+UPLOAD_FOLDER = './uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
+CORS(app)
+
+
+def get_predictions(image_file):
+    client = Client("midknight2002/happy_leaf")
+    print("CLIENT LOADED")
+    result = client.predict(
+        file(image_file),
+        api_name="/predict"
+    )
+    print("PREDICTION MADE")
+    print(result)
+    return result
+
+
+
+@app.errorhandler(Exception)
+def handle_error(err):
+    print(err)
+    message = str(err)
+    code = 500  # Internal Server Error (adjust based on error type)
+    return jsonify({'success': False, "error": message}), code
 
 @app.route('/')
 def index():
     return {
         "message": "Welcome to API for Planthealth AI"
     }
+
+@app.route("/api/predict_image", methods=["POST"])
+def predict_image():
+    if req.method == 'POST':
+        if 'image' not in req.files:
+            return jsonify({'success': False, 'error': 'Missing image file'}), 400  # Bad Request
+        image_file = req.files['image']
+        filename = secure_filename(image_file.filename)
+        path_ = os.path.join(app.config["UPLOAD_FOLDER"], filename )
+        image_file.save(path_)
+        p=get_predictions(path_)
+        print(p)
+        os.remove(path_)
+
+        return jsonify({'success': True }), 200  # OK
+
+    else:
+        return jsonify({'error': 'Method Not Allowed'}), 405  # Method Not Allowed
+
+
+app.run(debug=True)
